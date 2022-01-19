607e02c766cce6a3d08d7ea28f26bf09d63b0206
Nic Randazzo <nicmrandazzo@gmail.com>
2022-01-18 20:26:47 -0800

    Added image, sound, some more stuff

--- script.js
+++ script.js
@@ -4,25 +4,28 @@ function someFunction(){
     if(document.getElementById("center-button").style.opacity > 0){
         document.getElementById("center-button").style.opacity = 0;
         count++;
-        console.log(count);
     }
     else{
         document.getElementById("center-button").style.opacity = 100;
     }
     
     if(count == 10){
-        document.getElementById("ten-count").style.opacity = 100;
+        document.getElementById("ten-count").style.top = "20%";
+        document.getElementById("dot-clickable").style.top = "32%";
+        var audio = new Audio("sound/stop-clicking-button.mp3");
+        audio.play();
     }
     else if(count == 20){
-        document.getElementById("ten-count").style.opacity = 0;
-        document.getElementById("twenty-count").style.opacity = 100;
+        document.getElementById("dot-clickable").style.top = "-32%";
+        document.getElementById("ten-count").style.top = "-20%";
+        document.getElementById("twenty-count").style.top = "20%";
     }
     else if(count == 50){
-        document.getElementById("twenty-count").style.opacity = 0;
-        document.getElementById("fifty-count").style.opacity = 100;
+        document.getElementById("twenty-count").style.top = "-20%";
+        document.getElementById("fifty-count").style.top = "20%";
     }
     else if(count == 100){
-        document.getElementById("fifty-count").style.opacity = 0;
-        document.getElementById("hundred-count").style.opacity = 100;
+        document.getElementById("fifty-count").style.top = "-20%";
+        document.getElementById("hundred-count").style.top ="20%";
     }
 }
